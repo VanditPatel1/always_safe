dc3c1201893ccffb5e3df7d0080a3613aa005135
VanditPatel1 <patel.vandit1@gmail.com>
2018-03-29 15:33:50 -0400

    Functions to search for file in s3

--- backup.py
+++ backup.py
@@ -1,6 +1,21 @@
 from s3_connector import S3_Conn
+import pprint
+import os
 
+pp = pprint.PrettyPrinter(indent=4)
 
 s = S3_Conn()
 
-s.upload_file("test.txt", "/test/test.txt")
+s.upload_file("abc.txt", "l/test.txt")
+a = s.ls_all()
+b = s.ls_key(prefix='')
+
+
+pp.pprint(a)
+
+print ("-------")
+
+pp.pprint(b)
+
+
+def save_dir()
--- s3_connector.py
+++ s3_connector.py
@@ -1,6 +1,8 @@
 import boto3
 import time
-from AWS_CREDS import *
+
+""" Store this file in .gitignore or in enviroment outside git repo """
+from AWS_CREDS import DEFAULT_BUCKET, ACCESS_KEY_ID, SECRET_ACCESS_KEY
 
 
 class S3_Conn:
@@ -11,17 +13,35 @@ class S3_Conn:
         self.bucket = bucket or DEFAULT_BUCKET
         self.aws_id = ACCESS_KEY_ID
         self.aws_secret = SECRET_ACCESS_KEY
-        self.s3 = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
+        self._cli = boto3.resource('s3', aws_access_key_id=ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
+        self.s3 = self._cli.meta.client
 
 
     def upload_file(self, local_path, s3_path):
         print ("Starting file upload...")
-        start = time.time()
 
         try:
-            s3.meta.client.upload_file(local_path, self.bucket, s3_path)
+            start = time.time()
+            self.s3.upload_file(local_path, self.bucket, s3_path)
+            end = time.time()
+            print ("Upload took {0} seconds".format(end-start))
+
         except Exception as ex:
             print (ex)
-        
-        end = time.time()
-        print ("Upload took {0} seconds".format(end-start))
+            raise
+
+    def ls_all(self, bucket=None):
+        b = bucket or self.bucket
+        list_obj = self.s3.list_objects_v2(Bucket=b)
+        response = list_obj['Contents']
+        return response
+
+    def ls_key(self, prefix, bucket=None):
+        b = bucket or self.bucket
+        prefix = str(prefix) #boto3 expects a string
+
+        try:
+            list_obs = self.s3.list_objects_v2(Bucket=b, Prefix=prefix)
+        except Exception as ex:
+            print ("Error finding files with prefix: {0} in bucket: {1}".format(prefix, bucket))
+            raise
